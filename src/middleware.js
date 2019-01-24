03f4b1cf9da9bc6a1502d3e82fdaf62828a59da1
Tiberiu Simion Voicu <tiberiusimionvoicu@gmail.com>
2019-01-24 21:22:07 +0000

    clean middleware

--- src/middleware.js
+++ src/middleware.js
@@ -3,9 +3,6 @@ import helmet from 'helmet'
 import morgan from 'morgan'
 import dotenv from 'dotenv/config'
 import express from 'express'
-import bodyparser from 'body-parser'
-import jwt from 'jsonwebtoken'
-import config from './conf'
 
 export default (app) => {
     if(process.env.NODE_ENV === 'prod') {
@@ -13,8 +10,8 @@ export default (app) => {
         app.use(helmet())
     }
     
-    app.use(bodyparser.urlencoded({extended: true}))
-    app.use(bodyparser.json())
+    app.use(express.urlencoded({extended: true}))
+    app.use(express.json())
     app.use( (req, res, next) => {
         res.header('Access-Control-Allow-Origin', "*")
         res.header('Access-Control-Allow-Headers', "*")
